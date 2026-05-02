0fc774e01b64e360fb5aaf9e89d58c4ee197768e
Emily Perong <perongemily@gmail.com>
2026-05-02 17:31:55 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TimerApp from './TimerApp';
-import Blog from './pages/Blog';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -11,7 +10,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TimerApp />} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
