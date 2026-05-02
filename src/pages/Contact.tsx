205d6b352dbbf33dbb628f95d87a124d1c6eda00
Emily Perong <perongemily@gmail.com>
2026-05-02 20:13:51 +0800

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -36,7 +36,7 @@ export default function Contact() {
                 </div>
                 <div className="flex items-center text-slate-300">
                   <MapPin className="w-5 h-5 mr-3 text-emerald-400" />
-                  <span>Cognito Code, Bacolod, PH</span>
+                  <span>Cognito Code, Cebu City, PH</span>
                 </div>
               </div>
             </div>
