a0072efe3e2b1f98f7368b466a62e7d20ef3224c
Emily Perong <perongemily@gmail.com>
2026-05-13 15:35:05 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
+import BlogPost from './pages/BlogPost';
+
 export default function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<TimerApp />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
